d3f6f91482094a602a9fdee957d8ff6938d04323
Samuel Gratzl <sam@sgratzl.com>
2020-04-19 22:21:55 +0200

    better binder environment

--- binder/install.R
+++
@@ -1,6 +0,0 @@
-# mybinder.org install script
-
-install.packages('devtools')
-install.packages('rmarkdown')
-install.packages('tidyverse')
-devtools::install_url("https://github.com/upsetjs/upsetjs_r/releases/latest/download/upsetjs.tar.gz")
