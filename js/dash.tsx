c3b71e01dbc4a99beeb6a065d4b1556b0528ce35
Samuel Gratzl <sam@sgratzl.com>
2021-09-24 09:10:21 -0400

    feat: auto size

--- js/dash.tsx
+++ js/dash.tsx
@@ -8,11 +8,15 @@ import React from 'react';
 
 import { KarnaughMap, UpSetJS, VennDiagram } from '@upsetjs/react';
 import { createContext, fixProps, RBindingUpSetProps, adapter } from './model';
+import useResizeObserver from 'use-resize-observer';
 
-declare type DashUpSetJSProps = RBindingUpSetProps & {};
+declare type DashUpSetJSProps = RBindingUpSetProps & {
+  width: string | number | undefined;
+  height: string | number | undefined;
+};
 
-export function DashUpSetJS(props: DashUpSetJSProps) {
-  const context = createContext(300, 300); // TODO
+function DashUpSetJSImpl(props: RBindingUpSetProps) {
+  const context = createContext(props.width, props.height);
   fixProps(context, props);
   if (context.renderMode === 'venn') {
     delete context.props.layout;
@@ -24,4 +28,16 @@ export function DashUpSetJS(props: DashUpSetJSProps) {
     return <VennDiagram {...(context.props as any)} />;
   }
   return <UpSetJS {...(context.props as any)} />;
+}
+
+export function DashUpSetJS(props: React.PropsWithChildren<DashUpSetJSProps>) {
+  const { id, width = '100%', height = 300, children, ...rest } = props;
+  const { ref, width: computedWidth = 300, height: computedHeight = 300 } = useResizeObserver<HTMLDivElement>();
+  // TODO interaction
+  return (
+    <div ref={ref} id={props.id} style={{ width, height }}>
+      <DashUpSetJSImpl {...rest} width={computedWidth} height={computedHeight} />
+      {children}
+    </div>
+  );
 }
