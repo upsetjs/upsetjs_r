d46acde7faf196ebce574d783dafe1bbb989a87d
Samuel Gratzl <sam@sgratzl.com>
2022-12-30 21:09:33 -0500

    fix: linting

--- dash/basic.R
+++ dash/basic.R
@@ -6,21 +6,21 @@ library(upsetjs)
 app <- Dash$new()
 
 app$layout(
-    htmlDiv(
-        list(
-            htmlH1("Hello UpSet.js + Dash"),
-            upsetjsDash(id = "upset") %>% fromList(list(a = c(1, 2, 3), b = c(2, 3)))
-                %>% interactiveChart(),
-            htmlDiv(id = "output")
-        )
+  htmlDiv(
+    list(
+      htmlH1("Hello UpSet.js + Dash"),
+      upsetjsDash(id = "upset") %>% fromList(list(a = c(1, 2, 3), b = c(2, 3)))
+        %>% interactiveChart(),
+      htmlDiv(id = "output")
     )
+  )
 )
 app$callback(
-    output = list(id = "output", property = "children"),
-    params = list(input(id = "upset", property = "selection")),
-    function(selection) {
-        sprintf("You selected \"%s\"", selection$name)
-    }
+  output = list(id = "output", property = "children"),
+  params = list(input(id = "upset", property = "selection")),
+  function(selection) {
+    sprintf("You selected \"%s\"", selection$name)
+  }
 )
 
 app$run_server()
--- shiny/basic.R
+++ shiny/basic.R
@@ -17,9 +17,11 @@
 library(shiny)
 library(upsetjs)
 
-listInput <- list(one = c(1, 2, 3, 5, 7, 8, 11, 12, 13),
-                  two = c(1, 2, 4, 5, 10),
-                  three = c(1, 5, 6, 7, 8, 9, 10, 12, 13))
+listInput <- list(
+  one = c(1, 2, 3, 5, 7, 8, 11, 12, 13),
+  two = c(1, 2, 4, 5, 10),
+  three = c(1, 5, 6, 7, 8, 9, 10, 12, 13)
+)
 
 ui <- fluidPage(
   titlePanel("UpSet.js Shiny Example"),
@@ -29,10 +31,11 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   # render upsetjs as interactive plot
   output$upsetjs1 <- renderUpsetjs({
-    upsetjs() %>% fromList(listInput) %>% interactiveChart()
+    upsetjs() %>%
+      fromList(listInput) %>%
+      interactiveChart()
   })
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
-
